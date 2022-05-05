4ba9ded1a9e176f5d2bad4f9db2172f7ce6c3437
uvsq22103697 <lea.lapalut@ens.uvsq.fr>
2022-05-05 12:52:58 +0200

    mise en page finie et avancements

--- projet-mastermind.py
+++ projet-mastermind.py
@@ -7,3 +7,58 @@
 # https://github.com/uvsq22103697/projet_mastermind.git
 #########################################
 
+### Biblio
+
+import tkinter as tk
+import copy
+import random as rd
+
+#########################################
+### Constantes
+
+#########################################
+# Variables Glo et Widgets
+
+#########################################
+# Fonctions
+
+#########################################
+# Programme principal
+
+ # Définitions des widgets
+
+ # Placement des widgets
+
+ # Liaisons d'évènements
+
+ # Boucle principale
+racine.mainloop()
+
+#### EXEMPLE TROUV2 SUR INTERNET
+
+#Transformation d'une chaine en liste (def to_list(s))
+chaine = "123456"
+lst_chaine = list(chaine)
+print("chaine = ", chaine, "        liste = ", lst_chaine)
+
+#liste des couleurs des pions
+lst_init_couleurs = ["bleu", "rose", "vert", "jaune", "violet"]
+
+#Copie d'une liste dans une autre liste (def copie(liste):)
+lst_melange_couleurs = list(lst_init_couleurs)
+
+#Mélange aléatoire de la liste (def cache(n,k))
+shuffle(lst_melange_couleurs)
+
+print(lst_init_couleurs, "couleurs initiales")
+print(lst_melange_couleurs, "couleurs mélangées")
+
+#Controle couleur bien ou mal placée
+i = 0
+for element in lst_init_couleurs:
+    if lst_melange_couleurs[i] == element:
+        print("OK couleur", element,  "en position ", i)
+    else:
+        print("KO couleur en position ", i)
+
+    i += 1
